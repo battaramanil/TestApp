e32930f064e5119c5709c84341bd6f5d791f82c5
anil battaram <b.anilkumar81@gmail.com>
2020-02-13 01:30:01 +0530

    Changed root url

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Nagababu!'));
+app.get('/', (req, res) => res.send('Hello Anil Kumar Battaram!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}`));
