bd30c870bde24fdbbf9ede55a8832580105bb471
anil battaram <b.anilkumar81@gmail.com>
2020-02-13 01:47:51 +0530

    Testing

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Anil Kumar Battaram. How are you...!'));
+app.get('/', (req, res) => res.send('Hello Anil Kumar Battaram.!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}`));
